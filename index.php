151b634d01c54acc1442302ef7e747799e3308e4
faraji-fuji <farajifuji@gmail.com>
2022-05-22 09:29:39 +0300

    php files

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+include("header.php");
+?>
+
+<div class="container-fluid text-left">
+    <div class="row justify-content-evenly">
+        <div class="col-2">
+            <div class="list-group  shadow mb-5 bg-body rounded">
+                <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                    Crops
+                </a>
+                <a href="#" class="list-group-item list-group-item-action">Vegetables</a>
+                <a href="#" class="list-group-item list-group-item-action">Cereals</a>
+                <a href="#" class="list-group-item list-group-item-action">Fruits</a>
+                <a href="#" class="list-group-item list-group-item-action">Spices</a>
+                <a href="#" class="list-group-item list-group-item-action">Fruits</a>
+                <a href="#" class="list-group-item list-group-item-action">Legumes</a>
+                <a href="#" class="list-group-item list-group-item-action">Fruits</a>
+                <a href="#" class="list-group-item list-group-item-action">Legumes</a>
+                <a href="#" class="list-group-item list-group-item-action">Fruits</a>
+                <!-- <a class="list-group-item list-group-item-action disabled">Spices</a> -->
+            </div>
+        </div>
+        <div class="col-7">
+            <div id="carouselExampleCaptions" class="carousel slide shadow mb-5 bg-body rounded" data-bs-ride="false">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="https://picsum.photos/712/384" class="d-block w-100 rounded-top" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://picsum.photos/712/384" class="d-block w-100 rounded-top" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://picsum.photos/712/384" class="d-block w-100 rounded-top" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="list-group shadow mb-5 bg-body rounded">
+                <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                    Animal Products
+                </a>
+                <a href="#" class="list-group-item list-group-item-action">Eggs</a>
+                <a href="#" class="list-group-item list-group-item-action">Milk</a>
+                <a href="#" class="list-group-item list-group-item-action">Quails</a>
+                <a href="#" class="list-group-item list-group-item-action">Poultry</a>
+                <a href="#" class="list-group-item list-group-item-action">Milk</a>
+                <a href="#" class="list-group-item list-group-item-action">Fruits</a>
+                <a href="#" class="list-group-item list-group-item-action">Legumes</a>
+                <a href="#" class="list-group-item list-group-item-action">Fruits</a>
+                <a href="#" class="list-group-item list-group-item-action">Fruits</a>
+                <!-- <a class="list-group-item list-group-item-action disabled">Spices</a> -->
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include("footer.php");
+?>
