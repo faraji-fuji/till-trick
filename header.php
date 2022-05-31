f73f2a68b3deeaba1c4a816e089679d1ab3fea07
faraji-fuji <farajifuji@gmail.com>
2022-05-31 17:49:55 +0300

    register front-end

--- header.php
+++ header.php
@@ -7,13 +7,14 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    <title>CashCrop</title>
+    <link rel="stylesheet" href="css\register.css">
   </head>
   <body>
     <header class="sticky-top bg-success bg-gradient shadow">
       <div class="container">
         <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
-          <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-            <img src="images\cashcroplogo1.png" width="100" alt="" class="header-logo">
+          <a href="index.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+            <img src="images\cashcroplogo.png" width="100" alt="" class="header-logo">
           </a>
 
           <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
@@ -28,7 +29,7 @@
             <a href="login.php">
               <button type="button" class="btn btn-outline-light me-2">Login</button>
             </a>
-            <a href="">
+            <a href="register.php">
               <button type="button" class="btn btn-outline-light">Sign-up</button>
             </a>
           </div>
