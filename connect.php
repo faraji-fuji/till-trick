6113d358d198d66e98924c1ed3f2e9b5ae3fcd7b
Faraji Ombonya <farajiombonya@gmail.com>
2023-12-09 09:28:59 +0300

    fix: create table if not exists

--- connect.php
+++ connect.php
@@ -34,7 +34,7 @@ initialize_db($db_hostname, $db_username, $db_password);
 
 // mysqli queries to create database tables
 // user
-$query_create_table_user = "CREATE TABLE user(
+$query_create_table_user = "CREATE TABLE IF NOT EXISTS user(
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     email_address VARCHAR(50) PRIMARY KEY,
@@ -47,7 +47,7 @@ $query_create_table_user = "CREATE TABLE user(
 )";
 
 // product
-$query_create_table_product = "CREATE TABLE product(
+$query_create_table_product = "CREATE TABLE IF NOT EXISTS product(
     product_name VARCHAR(50),
     product_unit VARCHAR(50),
     product_price VARCHAR(50),
@@ -59,7 +59,7 @@ $query_create_table_product = "CREATE TABLE product(
 )";
 
 // order
-$query_create_table_order = "CREATE TABLE orders(
+$query_create_table_order = "CREATE TABLE IF NOT EXISTS orders(
     id INT AUTO_INCREMENT PRIMARY KEY,
     email_address VARCHAR(50),
     subtotal INT,
@@ -70,7 +70,7 @@ $query_create_table_order = "CREATE TABLE orders(
 )";
 
 // order_item
-$query_create_table_order_item = "CREATE TABLE order_item(
+$query_create_table_order_item = "CREATE TABLE IF NOT EXISTS order_item(
     id INT AUTO_INCREMENT PRIMARY KEY,
     product_id INT,
     order_id INT,
@@ -80,7 +80,7 @@ $query_create_table_order_item = "CREATE TABLE order_item(
 )";
 
 // transactions
-$query_create_table_transactions = "CREATE TABLE transactions(
+$query_create_table_transactions = "CREATE TABLE IF NOT EXISTS transactions(
     id INT AUTO_INCREMENT PRIMARY KEY,
     email_address VARCHAR(50),
     order_id INT,
@@ -88,7 +88,7 @@ $query_create_table_transactions = "CREATE TABLE transactions(
 )";
 
 // forum
-$query_create_table_forum = "CREATE TABLE forum(
+$query_create_table_forum = "CREATE TABLE IF NOT EXISTS forum(
     message_id INT AUTO_INCREMENT PRIMARY KEY,
     message_time DATETIME DEFAULT CURRENT_TIMESTAMP,
     message_content VARCHAR(1000),
@@ -96,13 +96,13 @@ $query_create_table_forum = "CREATE TABLE forum(
 )";
 
 // cart
-$query_create_table_cart = "CREATE TABLE cart(
+$query_create_table_cart = "CREATE TABLE IF NOT EXISTS cart(
     cart_id INT AUTO_INCREMENT PRIMARY KEY,
     cart_owner VARCHAR(50)
 )";
 
 // cart_item
-$query_create_table_cart_item = "CREATE TABLE cart_item(
+$query_create_table_cart_item = "CREATE TABLE IF NOT EXISTS cart_item(
     id INT AUTO_INCREMENT PRIMARY KEY,
     cart_item_id INT,
     cart_item_quantity INT,
@@ -110,7 +110,7 @@ $query_create_table_cart_item = "CREATE TABLE cart_item(
 )";
 
 // testimonial
-$query_create_table_testimonial = " CREATE TABLE testimonial(
+$query_create_table_testimonial = " CREATE TABLE IF NOT EXISTS testimonial(
     id INT AUTO_INCREMENT PRIMARY KEY,
     sender_name VARCHAR(50),
     sender_address VARCHAR(50),
@@ -119,7 +119,7 @@ $query_create_table_testimonial = " CREATE TABLE testimonial(
 )";
 
 // contact us
-$query_create_table_contact = " CREATE TABLE contact(
+$query_create_table_contact = " CREATE TABLE IF NOT EXISTS contact(
     id INT AUTO_INCREMENT PRIMARY KEY,
     sender_name VARCHAR(50),
     sender_address VARCHAR(50),
